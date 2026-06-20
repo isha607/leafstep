9b43823bd49d1e07045d5346563ef4e42fb070b3
isha607 <ishamirkute1@gmail.com>
2026-06-21 00:23:47 +0530

    Force inline tslib in nitro bundle for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,12 @@ export default defineConfig({
   },
   nitro: {
     preset: "vercel",
+    rollupConfig: {
+      external: [],
+    },
     externals: {
       inline: ["tslib"],
+      traceInclude: ["tslib"],
     },
   },
 });
